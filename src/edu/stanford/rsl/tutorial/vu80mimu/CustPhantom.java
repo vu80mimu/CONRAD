79ea749c7e16833934796a893d813d5211286d95
Alexandra Foehst <vu80mimu@stud.informatik.uni-erlangen.de>
2016-04-12 11:32:28 +0200

    Add whole package

---
+++ src/edu/stanford/rsl/tutorial/vu80mimu/CustPhantom.java
@@ -0,0 +1,13 @@
+package edu.stanford.rsl.tutorial.vu80mimu;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class CustPhantom extends Grid2D{
+
+	public CustPhantom(float[] buffer, int width, int height) {
+		super(buffer, width, height);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
